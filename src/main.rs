6b38d0008564c2560278944a6110d1db7b61d340
whoisprivalov <nikita.karatsev@gmail.com>
2021-05-03 23:38:04 +0600

    [~] permutation.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod permutation;
+
 fn main() {
     println!("Hello, world!");
 }
--- src/permutation.rs
+++ src/permutation.rs
@@ -1,12 +1,11 @@
-pub struct Permutation {
-    permutation : mut array;
-    length : u32;
-    parity : u32;
-    cycles : u32;
-    
-
+pub struct Permutation<T>{
+    permutation : Vec::new(),
+    length : usize,
+    parity : usize,
+    numberOfCycles : usize,
+    inversion : usize
 }
 
-pub fn new() -> Permutation {
+impl Permutation<T> {
 
 }
