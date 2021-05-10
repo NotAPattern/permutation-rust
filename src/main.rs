bf0caaf738112357866fdc9838a1f23742caf11a
OneTheGraph <nikita.karatsev@gmail.com>
2021-05-10 09:17:02 +0600

    [~] smth changes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod permutation;
 
+use std::cell::RefCell;
+
 fn main() {
   println!("Hello, world!");
   let array = [1,2,3,4,5];
   println!("Test: {:?}", test_array(&array));
 }
 
-fn test_array(array: &[usize]) -> &[usize] {
+fn test_array<T>(array: &[T]) -> &[T] {
   return array
 }
--- src/permutation.rs
+++ src/permutation.rs
@@ -1,19 +1,22 @@
 use std::collections::HashMap;
+use std::cell::RefCell;
 
 mod permutation {
 
+struct SomeError;
+
 #[derive(Debug)]
 pub struct Permutation {
-  permutation: Vec<usize>,
+  permutation: Vec<u32>,
   length: usize,
-  parity: usize,
-  number_of_cycles: usize,
-  inversion: usize
+  parity: u32,
+  number_of_cycles: u32,
+  inversion: u32
 }
 
 impl Permutation {
-  pub fn new<T>(&self, array: &mut[usize]) -> Permutation {
-    let return_permutation = Permutation{
+  pub fn new(&self, array: std::cell::RefCell<Vec<u32>>) -> Self {
+    let return_permutation = Permutation {
         permutation: self.create_permutation(array),
         length: self.length(), 
         parity: 0,
@@ -24,13 +27,13 @@ impl Permutation {
     return return_permutation
   }
 
-  fn create_permutation<'a>(&self, array: &mut [usize]) -> Vec<usize> {
-   return array.to_vec()
+  fn create_permutation(&self, array: std::cell::RefCell<Vec<u32>>) -> Vec<u32> {
+   return array.borrow().to_vec()
   }
 
   fn length(&self) -> usize {
     return self.permutation.len()
-  }
+  } 
 }
 
 }
